c89a40923c1be0a0ffeb2e7cdc991cddcfe9eb71
lcaparada <lcaparada@gmail.com>
2024-12-24 16:57:53 -0300

    fix: adjust splash screen

--- src/components/Splash/Splash.tsx
+++ src/components/Splash/Splash.tsx
@@ -1,18 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 
 import { AVPlaybackStatus, ResizeMode, Video } from "expo-av";
 import { hideAsync } from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
+import {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+
+import { useAppTheme } from "@hooks";
+
+import { AnimatedBoxRNR } from "../Box/Box";
 
 type SplashProps = {
+  loading: boolean;
   onComplete: (status: boolean) => void;
 };
 
-export const Splash = ({ onComplete }: SplashProps) => {
+export const Splash = ({ loading, onComplete }: SplashProps) => {
+  const [animationDone, setAnimationDone] = useState(false);
+
+  const opacity = useSharedValue(1);
+
   const [lastStatus, setStatus] = useState<AVPlaybackStatus>(
     {} as AVPlaybackStatus
   );
+
+  const theme = useAppTheme();
+  const FADE_ANIMATION_DURATION = 500;
 
   const onPlaybackStatusUpdate = (status: AVPlaybackStatus) => {
     if (status.isLoaded) {
@@ -20,23 +37,43 @@ export const Splash = ({ onComplete }: SplashProps) => {
         hideAsync();
       }
       if (status.didJustFinish) {
-        onComplete(true);
+        setAnimationDone(true);
       }
     }
     setStatus(() => status);
   };
 
+  const animatedStyle = useAnimatedStyle(() => ({
+    opacity: opacity.value,
+  }));
+
+  useEffect(() => {
+    if (animationDone && !loading) {
+      opacity.value = withTiming(0, {
+        duration: FADE_ANIMATION_DURATION,
+      });
+      setTimeout(() => onComplete(true), FADE_ANIMATION_DURATION);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [animationDone, loading]);
+
   return (
-    <>
+    <AnimatedBoxRNR
+      backgroundColor={"primary"}
+      style={[animatedStyle, StyleSheet.absoluteFill]}
+    >
       <StatusBar style="light" />
       <Video
         shouldPlay
         isLooping={false}
-        style={StyleSheet.absoluteFill}
+        style={[
+          StyleSheet.absoluteFill,
+          { backgroundColor: theme.colors.bluePrimary },
+        ]}
         resizeMode={ResizeMode.COVER}
         source={require("../../../assets/splash.mp4")}
         onPlaybackStatusUpdate={onPlaybackStatusUpdate}
       />
-    </>
+    </AnimatedBoxRNR>
   );
 };
