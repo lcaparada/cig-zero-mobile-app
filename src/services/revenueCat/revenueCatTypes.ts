9e1a396df3cf98ba0d1e75efe393e09828fab4b2
Lucas <cavalcantelucasx@gmail.com>
2024-12-31 17:05:48 -0300

    feat:remove comment code

--- src/services/revenueCat/revenueCatTypes.ts
+++ src/services/revenueCat/revenueCatTypes.ts
@@ -5,10 +5,6 @@ import {
 } from "react-native-purchases";
 
 export type RevenueCatOfferingMetadataData = { discount?: string };
-// export type RevenueCatOfferingMetadata = Record<
-//   string,
-//   RevenueCatOfferingMetadataData
-// >;
 
 export type PurchasePackageParams = {
   email: string;
@@ -22,23 +18,14 @@ export type SetAttributesParams = {
 
 export type RevenueCatService = {
   isLoading: boolean;
-  // paywallIsVisibled: boolean;
-  // showAllPlans: boolean;
   selectedPackage: string;
   packages: PurchasesPackage[];
   customerInfo: CustomerInfo | null;
-  // metadata: RevenueCatOfferingMetadata;
   currentSubscriptionIsVisibled: boolean;
   availableIntroPrice: null | PurchasesIntroPrice;
   loadProducts: () => Promise<void>;
-  // hidePaywall: () => void;
-  // setShowAllPlans: () => void;
   purchasePackage: () => Promise<void>;
   restorePurchases: () => Promise<void>;
-  // selectPackage: (identifier: string) => void;
   getCustomerInfo: () => Promise<CustomerInfo>;
-  // checkIfUserIsPremium: () => Promise<void>;
-  // showCurrentSubscriptionBottomSheet: () => void;
   hideCurrentSubscriptionBottomSheet: () => void;
-  // showPaywallOrCurrentSubscriptionBottomSheet: () => Promise<void>;
 };
